7a39520c90a0b61b802805b007f2019d7e364443
Antonio Haley <antonio.haley@gmail.com>
2014-03-05 23:15:12 -0500

    Refactored!

--- DroneStroker.CLI/DroneStriker.cpp
+++ DroneStroker.CLI/DroneStriker.cpp
@@ -28,34 +28,38 @@ DroneStriker::~DroneStriker()
 	}
 }
 
-vector<Point2f> DroneStriker::GetCenterMasses(Mat image)
+vector<Moments> GetMomentsFromContours(vector<vector<Point>> contours)
 {
-	Mat blackAndWhite;
-
-	inRange(image, Scalar(0, 0, 200), Scalar(0, 0, 255), blackAndWhite);
-
-	Mat cannyOutput;
-	const int thresh = 100;
-	Canny(blackAndWhite, cannyOutput, thresh, thresh << 1, 3);
-
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierarchy;
-
-	findContours(cannyOutput, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
-
 	vector<Moments> mu(contours.size());
 	for (int i = 0; i < contours.size(); i++) {
 		mu[i] = moments(contours[i], false);
 	}
+	return mu;
+}
 
+vector<Point2f> GetCenterMassFromContours(vector<vector<Point>> contours)
+{
+	vector<Moments> mu = GetMomentsFromContours(contours);
 	vector<Point2f> mc(contours.size());
 	for (int i = 0; i < contours.size(); i++) {
 		mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
 	}
+	return mc;
+}
 
+vector<Point2f> DroneStriker::GetCenterMasses(Mat image)
+{
+	Mat blackAndWhite;
+	inRange(image, Scalar(0, 0, 200), Scalar(0, 0, 255), blackAndWhite);
+	Mat cannyOutput;
+	const int thresh = 100;
+	Canny(blackAndWhite, cannyOutput, thresh, thresh << 1, 3);
+	vector<Vec4i> hierarchy;
+	vector<vector<Point>> contours;
+	findContours(cannyOutput, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
+	vector<Point2f> mc = GetCenterMassFromContours(contours);
 	imwrite("black-and-white.jpg", blackAndWhite);
 	imwrite("canny.jpg", cannyOutput);
-
 	return mc;
 }
 
